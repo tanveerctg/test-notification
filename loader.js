618c3073a001f528a4ba27eba45bd4ea1a247dae
Tanveer <37741388+tanveerctg@users.noreply.github.com>
2024-11-19 21:21:14 +0600

    loader js added

---
+++ loader.js
@@ -0,0 +1,7 @@
+(function () {
+  var script = document.createElement("script");
+  script.src =
+    "https://github.com/tanveerctg/test-notification/blob/main/notification.js"; // Your main notification script URL
+  script.async = true;
+  document.head.appendChild(script);
+})();
